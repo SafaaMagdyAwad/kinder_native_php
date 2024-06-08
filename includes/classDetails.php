e539248a911a0d1918296add3ede25819d46f687
SafaaMagdyAwad <139927841+SafaaMagdyAwad@users.noreply.github.com>
2024-06-08 20:49:36 +0300

    Add files via upload

---
+++ includes/classDetails.php
@@ -0,0 +1,86 @@
+<?php
+require_once "admin/includes/conn.php";
+
+if($_SERVER['REQUEST_METHOD'] == 'GET') {
+  try{
+    
+    $id=$_GET['id'];
+    $sql = "SELECT classes.* ,teachers.fullName FROM `classes` INNER JOIN `teachers`ON teachers.id=classes.teacher_id WHERE classes.id=?";
+    // var_dump($sql);
+    // die();
+    $stmt = $conn->prepare($sql);
+    $stmt->execute([$id]);
+    $classe= $stmt->fetch();
+  }catch(PDOException $e){
+      $error="error".$e->getMessage();
+  }
+}
+else{
+    header("location:404.php");
+}
+
+
+?>
+<div class="container my-5">
+        <div class="bg-light p-5 rounded">
+          <div class="card bg-light border-0">
+            <div class="row justify-content-center">
+            <h3>
+              <?php
+      if(isset($error)) {
+      ?>
+      <div style="color: #ee0002; padding: 5px;">
+        <?php echo $error ?>
+      </div>
+      <?php
+      }
+      ?>
+      </h3>
+              <div class="col-lg-4 col-md-6 col-10">
+                <img src="images/<?php echo $classe['image'] ?>" alt="" class="card-img" />
+              </div>
+              <div class="col-lg-8 col-md-6 col-12 card-body">
+                <div class="mb-4 text-center py-2">
+                  <h2 class="fw-semibold bg-light card-header"><?php echo $classe['className'] ?></h2>
+                </div>
+                <div class="mb-4">
+                  <p class="card-text">
+                    <span class="fw-bold">Teacher:</span> <?php echo $classe['fullName'] ?>
+                  </p>
+                </div>
+                <div class="mb-4">
+                  <p class="card-text">
+                    <span class="fw-bold">Price:</span> <?php echo $classe['price'] ?>$
+                  </p>
+                </div>
+                <div class="mb-4">
+                  <p class="card-text">
+                    <span class="fw-bold">Published:</span> <?php echo ($classe['published']==1)?"YES":"NO" ?>
+                  </p>
+                </div>
+                <div class="row">
+                  <div class="col-md-4" style="border-top: 3px solid #198754">
+                    <p class="text-success fs-5 fw-bold lh-1 pt-2">Age:</p>
+                    <p class="lh-1 fw-bold"><?php echo $classe['ageFrom'] ?> - <?php echo $classe['ageTo'] ?> Years</p>
+                  </div>
+                  <div class="col-md-4" style="border-top: 3px solid #fe5d37">
+                    <p class="text-primary fs-5 fw-bold lh-1 pt-2">Time:</p>
+                    <p class="lh-1 fw-bold"><?php echo $classe['timeFrom'] ?> - <?php echo $classe['timeTo'] ?> AM</p>
+                  </div>
+                  <div class="col-md-4" style="border-top: 3px solid #ffc107">
+                    <p class="text-warning fs-5 fw-bold lh-1 pt-2">Capacity:</p>
+                    <p class="lh-1 fw-bold"><?php echo $classe['capacity'] ?> kids</p>
+                  </div>
+                </div>
+                <div class="text-md-end">
+                  <a href="classes.php"
+                    class="btn mt-4 btn-primary text-white fs-5 fw-bold border-0 py-2 px-md-5"
+                  >
+                    Back to All classes
+                </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
